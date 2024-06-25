68819043aab164a19e45109f005f294759ec8d9e
Semenov Petr <64535714+UnderKarter@users.noreply.github.com>
2024-06-25 17:40:29 +0300

    Римско-Арабский Калькулятор.

---
+++ src/Converter.java
@@ -0,0 +1,94 @@
+import java.util.TreeMap;
+
+public class Converter {
+    TreeMap<Character, Integer> romanKeyMap = new TreeMap<>();
+    TreeMap<Integer, String> arabicKeyMap = new TreeMap<>();
+
+    public Converter() {
+        romanKeyMap.put('I', 1);
+        romanKeyMap.put('V', 5);
+        romanKeyMap.put('X', 10);
+        romanKeyMap.put('L', 50);
+        romanKeyMap.put('C', 100);
+        romanKeyMap.put('D', 500);
+        romanKeyMap.put('M', 1000);
+
+        arabicKeyMap.put(1000, "M");
+        arabicKeyMap.put(900, "CM");
+        arabicKeyMap.put(500, "D");
+        arabicKeyMap.put(400, "CD");
+        arabicKeyMap.put(100, "C");
+        arabicKeyMap.put(90, "XC");
+        arabicKeyMap.put(50, "L");
+        arabicKeyMap.put(40, "XL");
+        arabicKeyMap.put(10, "X");
+        arabicKeyMap.put(9, "IX");
+        arabicKeyMap.put(8, "VIII");
+        arabicKeyMap.put(7, "VII");
+        arabicKeyMap.put(6, "VI");
+        arabicKeyMap.put(5, "V");
+        arabicKeyMap.put(4, "IV");
+        arabicKeyMap.put(3, "III");
+        arabicKeyMap.put(2, "II");
+        arabicKeyMap.put(1, "I");
+    }
+
+    public boolean isRoman(String number){
+        return romanKeyMap.containsKey(number.charAt(0));
+    }
+
+    public String intToRoman(int number){
+        String roman = "";
+        int arabic;
+        do{
+            arabic = arabicKeyMap.floorKey(number);
+            roman += arabicKeyMap.get(arabic);
+            number -= arabic;
+        } while (number != 0);
+        return roman;
+
+    }
+
+    public int romanToInt(String s){
+
+        int end = s.length() - 1;
+        char[] arr = s.toCharArray();
+
+        int arabic;
+        int result = romanKeyMap.get(arr[end]);
+
+        int count = 0;
+        //Проверяем есть ли больше трех одинаковых знаков подряд
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i - 1] == arr[i]){
+                count++;
+            } else {
+                count = 0;
+            }
+            if (count >=3){
+                return result = 0;
+            }
+        }
+        //Проверяем есть ли больше двух одинаковых знаков перед больший знаком
+        if (arr.length >= 3){
+            int c = arr.length -1;
+            int b = arr.length -2;
+            int a = arr.length -3;
+            if (a == b || a < c){
+                return result = 0;
+            }
+        }
+
+        for (int i = end - 1; i >= 0; i--) {
+            arabic = romanKeyMap.get(arr[i]);
+
+            if (arabic < romanKeyMap.get(arr[i + 1])) {
+                result -= arabic;
+
+            } else {
+                result += arabic;
+            }
+        }
+        return result;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,99 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Converter converter = new Converter();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите выражение: ");
+        String input = scanner.nextLine();
+
+        String[] actions = {"+","-","*","/"};
+        String[] gotActions = {"\\+","-","\\*","/"};
+
+        //Определяем действие
+        int actionIndex = -1;
+        for (int i = 0 ; i < actions.length ; i++) {
+
+            if (input.contains(actions[i])) {
+                actionIndex = i;
+                break;
+            }
+
+        }
+
+        //Если не нашли знак выходим из программы:
+        if (actionIndex==-1 ){
+            System.out.println("Некорректное выражение");
+            return;
+        }
+        //Разделяем выражение
+        String[] data = input.split(gotActions[actionIndex]);
+
+        //Проверяем на кол-во арифметических знаков
+        if (data.length > 2){
+            System.out.println("Больше одного арифметическго действия");
+            return;
+        }
+
+        //Сравниваем формат выражений
+        if(converter.isRoman(data[0]) == converter.isRoman(data[1])){
+            int a,b;
+            //Определяем, римские ли это числа
+            boolean isRoman = converter.isRoman(data[0]);
+            if(isRoman){
+                //если римские, то конвертируем их в арабские
+                a = converter.romanToInt(data[0]);
+                b = converter.romanToInt(data[1]);
+
+                if (a == 0 || b == 0){
+                    System.out.println("В римском формате нет таких ");
+                    return;
+                }
+            }else{
+                //Значение арабских конвертируем в int
+                a = Integer.parseInt(data[0]);
+                b = Integer.parseInt(data[1]);
+
+            }
+
+            //Выполняем арифметическое действие
+            int result;
+            switch(actions[actionIndex]){
+                case "+":
+                    result = a+b;
+                    break;
+                case "-":
+                    result = a-b;
+                    break;
+                case "*":
+                    result = a*b;
+                    break;
+                default:
+                    result = a/b;
+                    break;
+            }
+
+            //Конвертируем арабские в римские
+            if(isRoman) {
+                //Результат от расчета римских цифр возвращаем в римском формате
+                if (result <= 0){
+                    System.out.println("В римском формате нет таких цифр");
+                } else {
+                    System.out.println(converter.intToRoman(result));
+                }
+
+            } else {
+                //Проверям диапазан чисел
+                if ((a > 10 || b > 10) || (a < 1 || b < 1)) {
+                    System.out.println("Арабские числа должны быть в диапазоне от 1 до 10 включительно");
+                } else {
+                    System.out.print(result);
+                }
+
+            }
+        } else {
+            System.out.println("Числа должны быть в одном формате");
         }
     }
 }
